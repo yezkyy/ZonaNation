4389cf833b17b61ce6e10aca0d1a3499e7e997c9
yezkyy <muhammadrizkys2004@gmail.com>
2025-01-03 00:17:00 +0800

    Refactor sidebar navigation icons for improved styling and user experience

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -2,20 +2,28 @@
     <div class="p-4">
         <nav class="space-y-8">
             <a href="/" class="flex flex-col items-center text-gray-700 hover:text-[#3F0071] transition">
-                <i class="fas fa-home text-4xl mb-1"></i>
-                <span class="text-sm">Home</span>
+                <div class="icon-container bg-gray-200 p-2 rounded-full shadow-md">
+                    <i class="fas fa-house-user text-3xl mb-1"></i>
+                </div>
+                <span class="text-sm mt-2">Home</span>
             </a>
             <a href="/transaksi" class="flex flex-col items-center text-gray-700 hover:text-[#3F0071] transition">
-                <i class="fas fa-exchange-alt text-4xl mb-1"></i>
-                <span class="text-sm">Transaksi</span>
+                <div class="icon-container bg-gray-200 p-2 rounded-full shadow-md">
+                    <i class="fas fa-exchange-alt text-3xl mb-1"></i>
+                </div>
+                <span class="text-sm mt-2">Transaksi</span>
             </a>
             <a href="/promo" class="flex flex-col items-center text-gray-700 hover:text-[#3F0071] transition">
-                <i class="fas fa-tags text-4xl mb-1"></i>
-                <span class="text-sm">Promo</span>
+                <div class="icon-container bg-gray-200 p-2 rounded-full shadow-md">
+                    <i class="fas fa-bullhorn text-3xl mb-1"></i>
+                </div>
+                <span class="text-sm mt-2">Promo</span>
             </a>
             <a href="/hadiahku" class="flex flex-col items-center text-gray-700 hover:text-[#3F0071] transition">
-                <i class="fas fa-gift text-4xl mb-1"></i>
-                <span class="text-sm">Hadiahku</span>
+                <div class="icon-container bg-gray-200 p-2 rounded-full shadow-md">
+                    <i class="fas fa-gift text-3xl mb-1"></i>
+                </div>
+                <span class="text-sm mt-2">Hadiahku</span>
             </a>
         </nav>
     </div>
